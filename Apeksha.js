a88da5fa5de6044ca63a17fc7061d37b79447e27
Apeksha60 <pavithriapeksha1new@gmail.com>
2023-05-20 12:56:14 +0530

    first commit

---
+++ Apeksha.js
@@ -0,0 +1,60 @@
+
+let name = "optimus";
+let year = 2023;
+console.log("Hello! My name is "+ name);
+console.log("I was created in "+ year);
+//Hello! My name is optimus
+//I was created in 2023
+
+//creat variable
+let name = "optimus";
+let year = 2023;
+name = "Niko";
+console.log("Hello! My name is "+ name);
+console.log("I was created in "+ year);
+//Hello! My name is Niko
+//I was created in 2023
+
+
+//Creat constant variable value 
+const fname = "ape";
+let year = 2023;
+console.log("Hello! My name is "+ fname);
+console.log("I was created in "+ year);
+//Hello! My name is ape
+//I was created in 2023
+
+//Find sum of 2 numbers
+let fnum = 5;
+let lnum = 10;
+console.log(fnum+lnum);
+//15
+
+//Find Area and perimeter of circle
+let r=7;
+const pi=3.147;
+perimeter = 2*pi*r;
+area = pi*r*r;
+console.log("Perimeter = " +perimeter);
+console.log("Area = " +area);
+//Perimeter = 44.058
+//Area = 154.203
+
+//Get input Value
+let r = prompt("radius value");
+const pi=3.14;
+perimeter = 2*pi*r;
+area = pi*r*r;
+radius value
+//615.44
+
+//Get input values
+typeof(num1) = string
+typeof(num2) = string
+let num1 = prompt("Enter first number");
+let num2 = prompt("Enter second number");
+let sum = num1 + num2 ;
+console.log("Sum =" + sum);
+//Enter first number 5 
+//Enter second number 15
+//Sum =515
