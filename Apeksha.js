b6093e1514b2f6e471492534cdecfe9bc5ab3be1
Apeksha60 <pavithriapeksha1new@gmail.com>
2023-05-27 10:13:23 +0530

    Binary arithmetic operators

--- Apeksha.js
+++ Apeksha.js
@@ -67,3 +67,15 @@ console.log(typeof("Apeksha"));     //string
 console.log(typeof(0.1));       //number
 console.log(typeof(true));      //boolean
 console.log(typeof('9'));       //string
+
+//Binary arithmetic operators
+console.log(12+26);     //38
+console.log(5+4.5);     //9.5
+console.log(5-3);       //2
+console.log(6-0.1);     //5.9
+console.log(10*4);      //40
+console.log(2*1.5);     //3
+console.log(8/2);       //4
+console.log(12/5);      //2.4
+console.log(10%3);      //1
+console.log(2**3);      //8
